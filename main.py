32a35f73d37d4b04c5435d1cdcae68cbc40e4574
Naqeeb Rahmani <naqeeb.rahmani@learnet.se>
2026-04-23 17:01:50 +0200

    The base game has been finished but there is a lot of room for improvements and the addition of other elements.

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ game_running = False
 game_mode = None # play or continue
 scene_nr = 0
 
+choice = None #False = choice 1 and True = choice 2
+
 ########################
 
 #---menu buttons---#
@@ -25,21 +27,43 @@ def exit_menu():
     in_menu = False
     game_running = True
     canvas.delete("all")
+    game_code()     
+
+def next_scene_1():
+    global scene_nr
+    global choice
+
+    if scene_nr == 2 and choice == True:
+        to_menu()
+    else:
+
+        choice = False
+    
+        scene_nr += 1
     game_code()
 
-def set_scene():
+def next_scene_2():
     global scene_nr
-    scene_nr = 0
+    global choice    
 
-def next_scene():
-    global scene_nr
-    if scene_nr < 5:
+    if scene_nr == 2 and choice == True:
+        to_menu()
+    else:
+
+        choice = True
+
         scene_nr += 1
+    game_code()
+
+def exit_menu_and_set_scene_1():
+    global scene_nr
+    scene_nr += 1
+    exit_menu()
 
 play = tk.Button(
     root, 
     text="Play", 
-    command=exit_menu,
+    command=exit_menu_and_set_scene_1,
     font=("Arial", 20),
     bg="#B0C4DE",
     width=10,
@@ -58,27 +82,47 @@ canvas.create_window(600, 300,anchor="center", window=play)
 ##########################
 
 #---scene variables---#
-scene0_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\Scene1.png"))
+scene1_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\Scene1.png"))
+
+scene2_c1_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\Scene2.png"))
+scene2_c2_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\you_died.png"))
+
+scene3_c1_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\scene3_if_conf.png"))
+scene3_c2_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\second_last_scene_if_sneaky.png"))
+
+scene4_c1_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\last_scene_if_escape.png"))
+scene4_c2_img = ImageTk.PhotoImage(Image.open("assets\Scene_pictures\last_scene_if_escape_sneaky.png"))
+
+scene_text_var = "Ugh... my head. What happened? Did i pass out? \nOh, no. Why isnt anyone moving. Could it be that i'm the only one alive?"
+
+
 
 choice_1 = tk.Button(
     root, 
     text="Look around", 
-    command=next_scene,
+    command=next_scene_1,
     font=("Arial", 20),
     bg="#B0C4DE",
-    width=10,
+    width=15,
     height=1
 )
 
 choice_2 = tk.Button(
     root, 
-    text="Play", 
-    command=exit_menu,
+    text="stay", 
+    command=next_scene_2,
     font=("Arial", 20),
     bg="#B0C4DE",
-    width=10,
+    width=15,
     height=1
 )
+
+def to_menu():
+    global in_menu, game_running
+    in_menu = True
+    game_running = False
+
+
 
 ##############################333333
 
@@ -91,72 +135,74 @@ choice_2 = tk.Button(
 
 
 
-
-
 #---game code---#
 def game_code():
-    if scene_nr == 0:
-        canvas.create_image(-50,0, anchor="nw", image=scene0_img)
-        #canvas.
+    global scene_text_var
+    
+    canvas.delete("all")
 
-    canvas.create_rectangle(
-        0, 550, 960, 720,
-        fill= "#B0C4DE",
-        outline="#E6E6FA",
-        width=4)
+    if in_menu == True and game_running == False:
+        canvas.create_image(-50,0, anchor="nw", image=menu_bg)
 
-    canvas.create_text(5, 600,
-    text= "What happened? Everyone is dead, but me\n my head",
-    fill="black", 
-    font=("Arial", 18),
-    anchor="nw")
+        canvas.create_window(600, 300,anchor="center", window=play)
 
-    canvas.create_window(1000, 600,anchor="nw", window=choice_1)
+    elif in_menu == False and game_running == True:
+
+        if scene_nr == 1:
+            canvas.create_image(-50,0, anchor="nw", image=scene1_img)
+
+
+        elif scene_nr == 2:
+            if choice == False:
+                canvas.create_image(-50,0, anchor="nw", image=scene2_c1_img)
+                scene_text_var = "A massive guard blocks the path. My friends are dead, and I’m a round target in a \nsquare city. I can try to find a heart in this machine, or I can trust only myself."
+                choice_1.config(text="ask for help")
+                choice_2.config(text="go the other way")
+                
+            else:
+                canvas.create_image(-50,-100, anchor="nw", image=scene2_c2_img)
+                choice_1.config(text="Menu")
+                scene_text_var = "No, i cant leave them. Not this time. \n*You died from hunger after around 24 hours."
+                
+        elif scene_nr == 3:
+            if choice == False:
+                canvas.create_image(-50,-100, anchor="nw", image=scene3_c1_img)
+                scene_text_var = "I step out. The guard’s eyes lock onto me. He leans down and whispers: \nI’ve heard stories of your kind... the Circle World. \nYou aren't safe here. There are others who might want to help. Follow me."
+                choice_1.config(text="Next")
+            else:
+                canvas.create_image(-50,0, anchor="nw", image=scene3_c2_img)
+                choice_1.config(text="Next")
+                scene_text_var = "I don't trust him. I slip back into the shadows and keep moving. \nAfter hours of wandering through the maze of gray blocks, the alley finally opens up. \nThere it is. A gleaming space facility and a spaceship as well, towering over the cubic city. \nIt’s my only way home."
+        elif scene_nr == 4:
+            if choice == False:
+                canvas.create_image(-50,-100, anchor="nw", image=scene4_c1_img)
+                choice_1.config(text="Menu")    
+                scene_text_var = "scene4_c1"
+            
+            else: 
+                canvas.create_image(-50,-100, anchor="nw", image=scene4_c2_img)
+                choice_2.config(text="Menu")
+                scene_text_var = "scene4_c2"      
+
+
+        canvas.create_rectangle(
+            0, 550, 1280, 720,
+            fill= "#B0C4DE",
+            outline="#E6E6FA",
+            width=4)
+        
+        scene_text = canvas.create_text(5, 567,
+            text= scene_text_var,
+            fill="black", 
+            font=("Arial", 18),
+            anchor="nw")
+        
+
+        canvas.create_window(1000, 560,anchor="nw", window=choice_1)
+        
+    
+        canvas.create_window(1000, 620,anchor="nw", window=choice_2)
+
+        print(choice)
 
 root.mainloop()
-
-
-
-#----------------extra-----------------#
-
-'''canvas.create_rectangle(
-    0, 550, 960, 720,
-    fill= "#B0C4DE",
-    outline="#E6E6FA",
-    width=4)
-
-canvas.create_text(5, 600,
-    text= "What happened? Everyone is dead, but me",
-    fill="black", 
-    font=("Arial", 18),
-    anchor="nw")
-
-def next_scene():
-    print("")'''
-
-
-'''next_btn = tk.Button(
-    root, 
-    text="Next →", 
-    command=next_scene,
-    font=("Arial", 14),
-    bg="#B0C4DE"
-)
-canvas.create_window(850, 680, window=next_btn)'''
-
-'''# Create empty text object
-text_obj = canvas.create_text(5, 600,
-    text= "", 
-    fill="black", 
-    font=("Arial", 18),
-    anchor="nw")
-
-# Typewriter function
-def type_text(text, index=0):
-    if index < len(text):
-        current = canvas.itemcget(text_obj, "text")
-        canvas.itemconfig(text_obj, text=current + text[index])
-        root.after(50, type_text, text, index + 1)
-
-# Start the effect
-type_text("What happened? Everyone is dead, but me")'''
